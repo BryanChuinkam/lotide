f313bbc5e90e47f128e636cf6453208902e37db1
bryanc.n <bryan.chuinkam@gmail.com>
2023-05-01 23:00:42 +0000

    added functionality to assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`💚️💚️💚️Assertion Passed: ${actual} === ${expected}.`);
+  } else {
+    console.log(`💔️💔️💔️Assertion Failed: ${actual} !== ${expected}.`);
+  }
+};
+
+// TEST CODE
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+assertEqual(1,41);
