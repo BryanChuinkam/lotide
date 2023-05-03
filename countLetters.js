4ab3e0dbe2f2dec4d3583e28a926e82d1d46f8a2
bryanc.n <bryan.chuinkam@gmail.com>
2023-05-03 18:25:08 +0000

    adding countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`💚️💚️💚️Assertion Passed: ${actual} === ${expected}.`);
+  } else {
+    console.log(`💔️💔️💔️Assertion Failed: ${actual} !== ${expected}.`);
+  }
+};
+
+
+
+const countLetters = function(sentence) {
+  let results = {};
+  let lowerCaseSentence = sentence.toLowerCase();
+  for (let letter of lowerCaseSentence) {
+    if (results[letter]) {
+      results[letter]++;
+    } else {
+      results[letter] = 1;
+    }
+  }
+  return results;
+};
+
+// Test
+let sentence = "lighthouse in the house";
+let countObj = countLetters(sentence);
+
+assertEqual(countObj['l'], 1);
+assertEqual(countObj['i'], 2);
+assertEqual(countObj['h'], 4);
+
