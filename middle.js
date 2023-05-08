cfed34fee3f7951da3824da5a8f3c1e5e029842b
bryanc.n <bryan.chuinkam@gmail.com>
2023-05-08 19:14:48 +0000

    refactored middle.js to make DRY and separated test into middleTest.js

--- middle.js
+++ middle.js
@@ -1,24 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) { // test edge case: when lengths aren't equal
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) { // if one element doesn't match return false
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`💚️💚️💚️Assertion Passed: ${array1} === ${array2}.`);
-  } else {
-    console.log(`💔️💔️💔️Assertion Failed: ${array1} !== ${array2}.`);
-  }
-};
-
-// ACTUAL FUNCTION
 const middle = function(array) {
   let arrayLength = array.length;
   let middleEleIndex;
@@ -36,10 +15,4 @@ const middle = function(array) {
   }
 };
 
-// TEST CODE
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1,2]), []);
-assertArraysEqual(middle([1,2,3]), [2]);
-assertArraysEqual(middle([1,2,3,4]), [2,3]);
-assertArraysEqual(middle([1,2,3,4,5]), [3]);
-assertArraysEqual(middle([1,2,3,4,5,6]), [3,4]);
+module.exports = middle; 
