8fe2b6b2ac811af6527b0923900c2fa788e789ef
bryanc.n <bryan.chuinkam@gmail.com>
2023-05-08 18:44:04 +0000

    refactored head.js and assertEqual.js to separate test and reduce duplication

--- head.js
+++ head.js
@@ -1,13 +1,6 @@
+const assertEqual = require('./assertEqual');
+
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`💚️💚️💚️Assertion Passed: ${actual} === ${expected}.`);
-  } else {
-    console.log(`💔️💔️💔️Assertion Failed: ${actual} !== ${expected}.`);
-  }
-};
-
-
 const head = function(array) {
   if (array) {
     return array[0];
@@ -15,10 +8,4 @@ const head = function(array) {
   return undefined;
 };
 
-
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5]), 5);
-assertEqual(head([]), 5);
-
+module.exports = head;
