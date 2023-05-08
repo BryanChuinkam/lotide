213798e9a10305d6cd2c4ad62b367bdf2160ea79
bryanc.n <bryan.chuinkam@gmail.com>
2023-05-08 22:03:11 +0000

    refactored middleTest and tailTest to use mocha and chai libraries and linted eqArraysTest

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,27 +1,28 @@
-const assertEqual = require("../assertEqual");
 const tail = require("../tail");
+const assert = require("chai").assert;
+
+describe("#tail", () => {
+  const arr1 = ["Hello", "Lighthouse", "Labs"];
+
+  it('it returns ["Lighthouse", "Labs"]  for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(arr1), ["Lighthouse", "Labs"]);
+  });
+
+  it('original array is unchanged', () => {
+    assert.deepEqual(arr1, ["Hello", "Lighthouse", "Labs"]);
+  });
 
 
-// Test Case 1: Check the returned array elements
-const array1 = ["Hello", "Lighthouse", "Labs"];
-const result1 = tail(array1);
-assertEqual(result1.length, 2); // ensure we get back two elements
-assertEqual(result1[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result1[1], "Labs"); // ensure second element is "Labs"
-assertEqual(array1.length, 3); // check original array
+  it('it returns [6, 7, 8]  for [5, 6, 7, 8]', () => {
+    assert.deepEqual(tail([5, 6, 7, 8]), [6, 7, 8]);
+  });
 
-// Test Case 2: Check the returned array elements
-const array2 = [5, 6, 7, 8];
-const result2 = tail(array2);
-assertEqual(result2.length, 3); // ensure we get back two elements
-assertEqual(result2[0], 6); // ensure first element is 6
-assertEqual(result2[1], 7); // ensure second element is 7
-assertEqual(array2.length, 4); // check original array
+  it('it returns []  for []', () => {
+    assert.isEmpty(tail([]));
+  });
 
-// Test Case 3: Empty array
-const result3 = tail([]);
-assertEqual(result3.length, 0);
+  it('it returns []  for [90]', () => {
+    assert.isEmpty(tail([90]));
+  });
+});
 
-// Test Case 3: One element in  array
-const result4 = tail([90]);
-assertEqual(result4.length, 0);
